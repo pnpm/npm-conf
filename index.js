c8c29d70353f6f0ac5d45fb71d9b4b70b862becb
Shipov Mikhail <mshipov@yandex.ru>
2019-03-07 08:16:43 +0300

    add check for require.resolve.paths existance

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ module.exports = (opts, types, defaults) => {
 
 	conf.add(Object.assign({}, opts), 'cli');
 
-	const paths = require.resolve.paths('npm');
-	// assume that last path in resolve paths is builtin modules directory
-	const npmPath = require.resolve('npm', { paths: paths.slice(-1) });
-	if (npmPath) {
-		/**
-		 *  according to https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/npm.js#L258
-		 *  and https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/config/core.js#L92
-		 */
-		conf.addFile(path.resolve(path.dirname(npmPath), '..', 'npmrc'), 'builtin');
+	if (require.resolve.paths) {
+		const paths = require.resolve.paths('npm');
+		// assume that last path in resolve paths is builtin modules directory
+		const npmPath = require.resolve('npm', {paths: paths.slice(-1)});
+		if (npmPath) {
+			/**
+			 *  according to https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/npm.js#L258
+			 *  and https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/config/core.js#L92
+			 */
+			conf.addFile(path.resolve(path.dirname(npmPath), '..', 'npmrc'), 'builtin');
+		}
 	}
 
 	conf.addEnv();
